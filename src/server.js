e415ef1c70aea60356b2fbc6c2e76a56f54ab0e5
Enzo Frantz <enzo.frantz@goeuro.com>
2017-07-16 23:36:15 +0200

    add multiple socket listeners

--- src/server.js
+++ src/server.js
@@ -1,31 +1,66 @@
 import hapi, { Server } from 'hapi';
 import nunjucks from 'nunjucks';
+import fetch from 'node-fetch'
+import backlogData from './mocks/backlog.mock';
 
 const server = new Server();
 server.connection({
   host: 'localhost',
-  port: 8067
+  port: 8010
 });
 
 const io = require('socket.io')(server.listener);
-// var io = require('socket.io').listen(server.listener);
 
 nunjucks.configure('./src/views');
 
 io.on('connect', function(socket) {
-  console.log('a user connected');
+  console.log('a user connected, current user count: ', Object.keys(io.engine.clients));
 
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    console.log('user disconnected, current user count: ', Object.keys(io.engine.clients));
   });
 });
 
-io.on('connection', function(socket){
+io.on('connection', socket => {
   socket.on('chat message', function(msg){
     console.log(msg);
     io.emit('chat message', msg);
   });
+
+  socket.on('vote', msg => {
+    console.log('starting vote', msg);
+    io.emit('vote start', 'started');
+    const sendMessage = (topic, message) => io.emit(topic, message);
+    startCountdown(sendMessage);
+  });
+
+  let unestimatedStories;
+  socket.on('request backlog', msg => {
+    unestimatedStories = backlogData.issues.map(m => ({ key: m.key, summary: m.fields.summary, description: m.fields.description }));
+    io.emit('backlog received', unestimatedStories);
+  });
+
+  socket.on('select current story', key => {
+    const currentStory = unestimatedStories.filter(f => f.key === key);
+    unestimatedStories = unestimatedStories.filter(f => f.key !== key);
+    io.emit('current story updated', currentStory);
+    io.emit('backlog received', unestimatedStories);
+  });
+
 });
+
+const startCountdown = (sendMessage, seconds = 10, interval = 500) => {
+	let remainingSeconds = seconds;
+	const countdownInterval = setInterval(() => {
+    sendMessage('vote countdown', remainingSeconds);
+    if (remainingSeconds <= 0) {
+      clearInterval(countdownInterval)
+      sendMessage('vote end', 'vote ended');
+    }
+		remainingSeconds--;
+	}, interval)
+
+}
 
 const startServer = () => {
 
